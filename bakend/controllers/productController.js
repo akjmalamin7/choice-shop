4bed8bd86617fdac40bcdb5044dd61a7ac5a4ba5
akjmalamin7 <thpi205526@gmail.com>
2023-03-10 18:27:17 +0600

    product create

--- bakend/controllers/productController.js
+++ bakend/controllers/productController.js
@@ -1,8 +1,39 @@
 const _ = require('lodash')
+const fs = require('fs')
 const { Product, validate } = require('../models/product')
+const formidable = require('formidable')
+const { result } = require('lodash')
 
 module.exports.createProduct = async(req, res) =>{
-    
+    const form = new formidable.IncomingForm()
+    form.keepExtensions = true
+    form.parse(req, (err, fields, files)=>{
+        if(err) return res.status(400).send("Something went wrong!")
+
+        const {error} = validate(_.pick(fields, ["name", "description", "price", "category", "quantity"]))
+        if(error) return res.status(400).send(error.details[0].message)
+
+        const product = new Product(fields)
+
+        if(files.photo){
+            fs.readFile(files.photo.path, (err, data)=>{
+                if(err) return res.status(400).send("Problem in file data!")
+
+                product.photo.data = data
+                product.contentType = files.photo.type 
+
+                product.save((err, result)=>{
+                    if(err) return res.status(500).send("Internal server error!")
+                    else return res.status(201).send({
+                        message:"Product created successfully!",
+                        data: _pick(result, ["name", "description", "price", "category", "quantity"])
+                    })
+                })
+            })
+        }else{
+            return res.status(400).send("No image provided!")
+        }
+    })
 }
 module.exports.getProducts = async(req, res) =>{
 
